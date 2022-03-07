cf95483e8549bed6925b080b82bf2316c7e7a193
Kone Labs <mvg210@gmail.com>
2022-03-07 14:30:32 -0800

    memo explicit

--- src/routes/well-known/index.ts
+++ src/routes/well-known/index.ts
@@ -93,7 +93,7 @@ router.get('/lnurlp/:username', async (req, res) => {
       const invoice = await lightningApi.lightningAddInvoice({
         value_msat: msat as string,
         r_preimage: preimage.toString('base64'),
-        memo: haloAddress,
+        memo: `haloAddress: ${haloAddress}`,
         description_hash: createHash('sha256').update(username).digest('base64')
       });
       // logger.debug('LND Invoice', invoice);

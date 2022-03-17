5c94ab1128f4863850d3ef1a5f2d1aacb5662d87
Kone Labs <mvg210@gmail.com>
2022-03-16 18:45:36 -0700

    Made invoice a comlpaint JSON object

--- src/routes/well-known/index.ts
+++ src/routes/well-known/index.ts
@@ -87,7 +87,7 @@ router.get('/lnurlp/:username', async (req, res) => {
       const invoice = await lightningApi.lightningAddInvoice({
         value_msat: msat as string,
         r_preimage: preimage.toString('base64'),
-        memo: `{"haloScope":"${haloScope}", "contentHash": ${username}`
+        memo: `{"haloScope":"${haloScope}", "contentHash": "${username}"`
         // description_hash: createHash('sha256').update(username).digest('base64')
       });
       // logger.debug('LND Invoice', invoice);
